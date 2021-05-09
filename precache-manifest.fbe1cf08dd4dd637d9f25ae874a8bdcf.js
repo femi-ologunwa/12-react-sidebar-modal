5de672b255472a3ec663e0c8d5e67492700625c7
Femi <ologunwafemi@gmail.com>
2021-05-09 10:26:15 +0100

    Updates

---
+++ precache-manifest.fbe1cf08dd4dd637d9f25ae874a8bdcf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a3254f3ebf111533cd3adebe0b88e2b",
+    "url": "/12-react-sidebar-modal/index.html"
+  },
+  {
+    "revision": "49dd84c42aea1427b362",
+    "url": "/12-react-sidebar-modal/static/css/main.e0f79945.chunk.css"
+  },
+  {
+    "revision": "4af09b5a46f0feaf1c0b",
+    "url": "/12-react-sidebar-modal/static/js/2.742e6281.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/12-react-sidebar-modal/static/js/2.742e6281.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49dd84c42aea1427b362",
+    "url": "/12-react-sidebar-modal/static/js/main.884f1e5f.chunk.js"
+  },
+  {
+    "revision": "9bc8a29051a8c3027367",
+    "url": "/12-react-sidebar-modal/static/js/runtime-main.18b4b5b5.js"
+  },
+  {
+    "revision": "2bb7da653fb63433560ef10511c9ae9d",
+    "url": "/12-react-sidebar-modal/static/media/logo.2bb7da65.svg"
+  }
+]);
