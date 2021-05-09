9658bb7806d2fb15ca442304e5ac3358e4dcca57
Femi <ologunwafemi@gmail.com>
2021-05-09 09:29:57 +0100

    created context for data sharing amongst components

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,11 @@
 import React, { useContext } from 'react';
 import { FaBars } from 'react-icons/fa';
+import { AppContext } from './context';
 
 const Home = () => {
+   const data = useContext(AppContext);
+   console.log(data); //renders 'Hello' from context.js
+
    return (
       <main>
          <button className='sidebar-toggle'>
--- src/context.js
+++ src/context.js
@@ -1,1 +1,11 @@
-import React, { useState, useContext } from 'react'
+import React, { createContext } from 'react';
+
+//create the context
+const AppContext = createContext();
+
+//create a component that will wrap around the parent component for which data & functions is to be shared among its children.
+const AppProvider = ({ children }) => {
+   return <AppContext.Provider value='hello'>{children}</AppContext.Provider>;
+};
+
+export { AppContext, AppProvider };
