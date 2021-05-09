e4e4fb105f17ee89812a5dc34a996f708aea1316
Femi <ologunwafemi@gmail.com>
2021-05-09 09:53:10 +0100

    setup data to be passed from context to components

--- src/context.js
+++ src/context.js
@@ -1,11 +1,47 @@
-import React, { createContext } from 'react';
+import React, { useState, createContext } from 'react';
 
 //create the context
 const AppContext = createContext();
 
 //create a component that will wrap around the parent component for which data & functions is to be shared among its children.
 const AppProvider = ({ children }) => {
-   return <AppContext.Provider value='hello'>{children}</AppContext.Provider>;
+   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+   const [isModalOpen, setIsModalOpen] = useState(false);
+
+   //function to open sidebar
+   const openSidebar = () => {
+      setIsSidebarOpen(true);
+   };
+
+   //function to close sidebar
+   const closeSidebar = () => {
+      setIsSidebarOpen(false);
+   };
+
+   //function to open modal
+   const openModal = () => {
+      setIsModalOpen(true);
+   };
+
+   //function to close modal
+   const closeModal = () => {
+      setIsModalOpen(false);
+   };
+
+   return (
+      <AppContext.Provider
+         value={{
+            isSidebarOpen,
+            isModalOpen,
+            openSidebar,
+            closeSidebar,
+            openModal,
+            closeModal,
+         }}
+      >
+         {children}
+      </AppContext.Provider>
+   );
 };
 
 export { AppContext, AppProvider };
